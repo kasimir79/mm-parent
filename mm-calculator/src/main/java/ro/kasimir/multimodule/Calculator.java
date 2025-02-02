4892b929767b014a6b849b301aa081d9cbdf323b
Kasimir Halitchi <kasimir.romania@gmail.com>
2025-02-02 13:23:29 +0200

    5th Commit

--- mm-calculator/src/main/java/ro/kasimir/multimodule/Calculator.java
+++ mm-calculator/src/main/java/ro/kasimir/multimodule/Calculator.java
@@ -2,7 +2,7 @@ package ro.kasimir.multimodule;
 
 public class Calculator {
 
-    public int add(int num14, int num2){
-        return num14+ num2;
+    public int add(int num15, int num2){
+        return num15+ num2;
     }
 }
